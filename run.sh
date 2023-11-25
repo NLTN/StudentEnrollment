36bd3b030e6806401a01e0d02e9f503bbce5eb87
Nguyen Nguyen <nltn@msn.com>
2023-11-24 22:42:19 -0800

    using Watchdog to monitor the services+create DBs

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 # Create .env file
 sh ./bin/create-dotenv.sh
 
-# Create user database if not exists
-sh ./bin/create-user-db.sh
+# Start Watchdog to detect the services are already running 
+# and create databases (if needed).
+python3 ./bin/watchdog.py &
 
 # Start the services
 # foreman start -m gateway=1,enrollment_service=3,user_service=1,dynamodb=1,redis=1
